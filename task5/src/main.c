7f177e49c0e04902616bc43142da04110fcbf2cf
d.mustakaev <d.mustakaev@g.nsu.ru>
2023-05-16 00:44:10 +0700

    v2.1.2 add printError func

--- task5/src/main.c
+++ task5/src/main.c
@@ -14,9 +14,7 @@ typedef enum error_code {
     bad_number_of_vertices,
     bad_number_of_edges,
     bad_vertex,
-    bad_length,
 } error_code;
-
 
 void initializeGraph() {
     int i, j;
@@ -68,43 +66,6 @@ int findShortestCycle(int numVertices) {
     }
 }
 
-//int readInput(int numVertices, int numEdges, const char *in_stream) {
-//    FILE *fp = fopen(in_stream, "r");
-//    if (!fp) {
-//        return other_error;
-//    }
-//    if (fscanf(fp, "%d", &numVertices) != 1) {
-//        fclose(fp);
-//        return bad_number_of_lines;
-//    }
-//    if (numVertices < 0 || numVertices > MAX_VERTICES) {
-//        fclose(fp);
-//        return bad_number_of_vertices;
-//    }
-//    if (fscanf(fp, "%d", &numEdges) != 1) {
-//        fclose(fp);
-//        return bad_number_of_lines;
-//    }
-//    if (numEdges < 0 || numEdges > numVertices * (numVertices - 1) / 2) {
-//        fclose(fp);
-//        return bad_number_of_edges;
-//    }
-//    int from, to, value;
-//    for (int i = 0; i < numEdges; ++i) {
-//        if (fscanf(fp, "%d %d", &from, &to) != 2) {
-//            fclose(fp);
-//            return bad_number_of_lines;
-//        }
-//        if (from < 0 || to < 0 || from > numVertices || to > numVertices) {
-//            fclose(fp);
-//            return bad_vertex;
-//        }
-//        addEdge(from, to);
-//    }
-//    fclose(fp);
-//    return no_error;
-//}
-
 int readInput(int* numVertices, int* numEdges, const char* in_stream) {
     FILE* fp = fopen(in_stream, "r");
     if(!fp)
@@ -141,12 +102,37 @@ int readInput(int* numVertices, int* numEdges, const char* in_stream) {
     return no_error;
 }
 
+int printError(error_code error, const char *out_stream) {
+    FILE *fp = fopen(out_stream, "w");
+    if (!fp)
+        return other_error;
+    if (error == bad_number_of_lines) {
+        fprintf(fp, "bad number of lines");
+    }
+    if (error == bad_number_of_vertices) {
+        fprintf(fp, "bad number of vertices");
+    }
+    if (error == bad_number_of_edges) {
+        fprintf(fp, "bad number of edges");
+    }
+    if (error == bad_vertex) {
+        fprintf(fp, "bad vertex");
+    }
+    fclose(fp);
+    return no_error;
+}
+
+
 int main() {
     int numVertices, numEdges;
-    const char* in_stream = "in.txt";
+    const char* in_stream = "in.txt", *out_stream = "out.txt";
     initializeGraph();
 
-    readInput(&numVertices, &numEdges, in_stream);
+    error_code error = readInput(&numVertices, &numEdges, in_stream);
+    if(error != no_error){
+        printError(error, out_stream);
+        return 0;
+    }
     int shortestCycleLength = findShortestCycle(numVertices);
 
     if (shortestCycleLength == -1) {
